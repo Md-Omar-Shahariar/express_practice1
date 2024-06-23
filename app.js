3409dd8d48c94cf0f94c63994a18d7e013c818a6
unknown <osafridi00@gmail.com>
2024-06-23 21:55:27 +0600

    router

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 const express = require("express");
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded());
-app.use("/", (req, res, next) => {
-  console.log("This Always runs");
-  // res.send("<h1>Hello From Express</h1>");
-  next();
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware");
-  res.send(
-    "<form action='/product' method='POST'><input name='title'  type='text'><button type='submit'>Add Product</button></form>"
-  );
-});
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("This Always runs");
-  res.send("<h1>Hello From Express</h1>");
-});
 app.listen(3000);
