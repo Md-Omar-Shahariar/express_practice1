4bef2c35fb62ef26e9deb3b989711b39f28b4585
unknown <osafridi00@gmail.com>
2024-06-23 20:00:26 +0600

    body parser

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const http = require("http");
 const express = require("express");
 const app = express();
-const server = http.createServer(app);
-server.listen(3000);
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
+app.use("/", (req, res, next) => {
+  console.log("This Always runs");
+  // res.send("<h1>Hello From Express</h1>");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In the middleware");
+  res.send(
+    "<form action='/product' method='POST'><input name='title'  type='text'><button type='submit'>Add Product</button></form>"
+  );
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("This Always runs");
+  res.send("<h1>Hello From Express</h1>");
+});
+app.listen(3000);
