3ebe007c76cac7bdacc6d026dec9ca5194bd11d8
unknown <osafridi00@gmail.com>
2024-06-25 01:12:30 +0600

    filter

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
+
+// filtering path
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
 
 app.listen(3000);
