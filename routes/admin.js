ed948c31faf5ff39196657a370c2b30b8bed6433
unknown <osafridi00@gmail.com>
2024-07-02 21:54:58 +0600

    file path

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,14 @@
 const express = require("express");
 
 const router = express.Router();
+const path = require("path");
 
 router.get("/add-product", (req, res, next) => {
-  console.log("In the middleware");
-  res.send(
-    "<form action='/admin/add-product' method='POST'><input name='title'  type='text'><button type='submit'>Add Product</button></form>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-  res.redirect("/");
+  res.redirect("/admin/add-product");
 });
 
 module.exports = router;
