cb49bb3ced83f1a02bcb22970392287c0a753c5e
unknown <osafridi00@gmail.com>
2024-07-03 18:50:52 +0600

    helper function

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const rootDir = require("../helper/path");
 const path = require("path");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../views/shop.html"));
+  res.sendFile(path.join(rootDir, "views/shop.html"));
 });
 
 module.exports = router;
